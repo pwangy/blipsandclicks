2e80846393c7983b909f18c97f0becb52bfad508
pwangy <blipsandclicks@gmail.com>
2024-07-16 23:13:42 -0700

    placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export const App = () => {
+	return (
+		<>
+			<section>
+				<div>
+					<h1>Hello!</h1>
+				</div>
+				<div className='update'>
+					<p>
+						I&apos;m a Frontend Developer and Designer based in
+						Berkeley, California.
+					</p>
+					<p>Bits and baubles are forthcoming.</p>
+					<p>In the meantime, you might visit:</p>
+					<p className='links'>
+						<a href='https://www.linkedin.com/in/peggywang/'>
+							LinkedIn
+						</a>
+						&nbsp;//&nbsp;
+						<a href='https://github.com/pwangy'>Github</a>
+						&nbsp;//&nbsp;
+						<a href='https://pwangy-portfolio.netlify.app/'>
+							Portfolio
+						</a>
+					</p>
+				</div>
+			</section>
+		</>
+	)
 }
-
-export default App
